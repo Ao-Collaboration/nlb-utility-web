fbe2fa0dc6a02a706388f8c4f91d6263d3b654bb
Michael Standen <michael@aocollab.tech>
2022-06-16 13:47:46 +1200

    Update rinkeby contract

--- src/config/chain.tsx
+++ src/config/chain.tsx
@@ -1,5 +1,5 @@
 export const getNlbContractId = (chainId: number): string => chainId === 4 ? '0xF5D3fDEa6b3434A0E739E8db9518e82E1820ED10' : '0x0e29086b5a3c8a0abd0662f5f1a4be2bee158058'
-export const getStakingContractId = (chainId: number): string => chainId === 4 ? '0xfc916f9bBb1c3a7E281B08f6152665398F58B814' : ''
+export const getStakingContractId = (chainId: number): string => chainId === 4 ? '0x504F1593EfDc4C9E0d0CA74514804200c0A33bf8' : ''
 
 export const infuraId = '288d3145859e45dca9d1c701fce7113e'
 export const validChainIds = [1, 4]
