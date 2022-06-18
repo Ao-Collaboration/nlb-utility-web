554cc2b9a620f0cf8fe3aafff3db9cfcc36a0f23
Michael Standen <michael@aocollab.tech>
2022-06-18 12:26:12 +1200

    Add CHOW token mainnet

--- src/config/chain.tsx
+++ src/config/chain.tsx
@@ -1,5 +1,5 @@
 export const getNlbContractId = (chainId: number): string => chainId === 4 ? '0xF5D3fDEa6b3434A0E739E8db9518e82E1820ED10' : '0x0e29086b5a3c8a0abd0662f5f1a4be2bee158058'
-export const getStakingContractId = (chainId: number): string => chainId === 4 ? '0x504F1593EfDc4C9E0d0CA74514804200c0A33bf8' : ''
+export const getStakingContractId = (chainId: number): string => chainId === 4 ? '0x504F1593EfDc4C9E0d0CA74514804200c0A33bf8' : '0xf7f1ca313a595c745dbb2c2ee1d58e6398861043'
 
 export const infuraId = '288d3145859e45dca9d1c701fce7113e'
 export const validChainIds = [1, 4]
